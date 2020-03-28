5cb323477418084f0cc47e7d9ab682cb88304dc3
sn3id3r <sneider_garcia88@hotmail.com>
2020-03-28 00:07:44 -0500

    Cambios JCLP

--- app/src/main/java/e/lil/myapplication/LoginActivity.java
+++ app/src/main/java/e/lil/myapplication/LoginActivity.java
@@ -1,12 +1,14 @@
 package e.lil.myapplication;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,11 +29,25 @@ public class LoginActivity extends AppCompatActivity {
     DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
     DatabaseReference mensajeRef = reference.child("Mensaje").child("Ultimo");
 
+    RelativeLayout rellay1, rellay2;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
 
+        handler.postDelayed(runnable, 2000);
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
